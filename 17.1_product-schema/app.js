940729cfd72551367ab5d11a2aa53aa1e4c9c223
svetlanagaydey <lana84@ukr.net>
2022-01-18 23:58:46 +0200

    exercise completed

---
+++ 17.1_product-schema/app.js
@@ -0,0 +1,22 @@
+const { connect } = require("mongoose");
+const Product = require("./productSchema.js");
+const products = require("./products.js")
+
+connect(
+  "mongodb://localhost/e-commerce",
+  () => {
+    console.log("mongoDB connected");
+  },
+  (e) => console.error(e)
+);
+const addProducts = async (products) => {
+    try {
+      const createProducts = await Product.create(products);
+      console.log(createProducts);
+      console.log("Products Created");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+addProducts(products);
---
+++ 17.1_product-schema/products.js
@@ -0,0 +1,39 @@
+const products = [
+    {
+        name: "Barbie dall",
+        category: "Toys",
+        isActive: true,
+        details: {
+          description: "Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem ",
+          price: 120,
+          discount: 20,
+          images: ["img1", "img2", "img3"],
+          phone: 55555555
+        }
+    },
+    {
+        name: "Hat",
+        category: "headdress",
+        isActive: true,
+        details: {
+          description: "Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem ",
+          price: 18,
+          discount: 20,
+          images: ["img1", "img2", "img3"],
+          phone: 55555555,
+        }
+    },
+    {
+        name: "keyboard",
+        category: "computer accessories",
+        isActive: false,
+        details: {
+          description: "Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem ",
+          price: 25,
+          images: ["img1", "img2", "img3"],
+          phone: 55555555,
+        }
+    }
+];
+
+module.exports = products;
