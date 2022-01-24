2cd5d0b615d61733936a163418cd9a1ecb0969d0
svetlanagaydey <lana84@ukr.net>
2022-01-24 14:39:38 +0200

    authenticatin function added

--- Self_learning/middleware/auth.js
+++ Self_learning/middleware/auth.js
@@ -3,9 +3,11 @@ const User = require('../models/user');
 
 const auth = async(req, res, next) => {
   try {
+    //console.log("auth middleware")
     const token = req.header('Authorization').replace('Bearer ', '');
+    console.log(token);
     const decoded = jwt.verify(token, 'mysecretfortoken');
-    const user = await User.findOne({_id: decoded._id, "tokens.token": token});
+    const user = await User.findOne({_id: decoded._id, "token": token});
 
     if (!user) {
         throw new Error();
@@ -13,7 +15,7 @@ const auth = async(req, res, next) => {
     req.user = user;
     next();
   } catch (e) {
-    res.status(410).sent({error: 'Please authenticate'})
+    res.status(410).send({error: 'Please authenticate'})
   }
 }
 module.exports = auth
--- Self_learning/routers/user.js
+++ Self_learning/routers/user.js
@@ -25,11 +25,17 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users/me', auth, async (req, res) => {
-    res.send(req.user)
+router.get('/users/me', auth, async (req, res) => { //profile
+    try {
+        const users = await User.find();
+        res.send(users)
+    }
+    catch(e) {
+        res.status(500).send()
+    }
 })
 
-router.get('/users', async (req, res) => {
+router.get('/users', auth, async (req, res) => {
     try {
         const users = await User.find();
         res.send(users);
