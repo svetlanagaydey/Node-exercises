f402aa97621536ef9b9d009438bf57d35a73aa97
svetlanagaydey <lana84@ukr.net>
2022-01-24 16:00:03 +0200

    authentication tokens, middleware function added

--- Self_learning/routers/user.js
+++ Self_learning/routers/user.js
@@ -25,14 +25,8 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users/me', auth, async (req, res) => { //profile
-    try {
-        const users = await User.find();
-        res.send(users)
-    }
-    catch(e) {
-        res.status(500).send()
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users', auth, async (req, res) => {
